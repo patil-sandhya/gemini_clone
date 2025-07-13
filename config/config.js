a292d507fff5d3749b6483d9733a57324896c834
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-07-13 21:54:15 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,15 +8,9 @@ module.exports = {
     host: process.env.DATABASE_HOST || 'localhost',
     dialect: 'postgres',
   },
-  // production: {
-  //   use_env_variable: 'DATABASE_URL',
-  //   dialect: 'postgres'
-  // }
-production: {
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  dialect: 'postgres',
-},
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+
 };
