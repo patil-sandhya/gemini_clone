826d94efe05a21b71cf7f0cf3743f7aeeb254c56
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-07-13 22:03:30 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -10,7 +10,13 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres'
+    dialect: 'postgres',
+     dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
   }
 
 };
