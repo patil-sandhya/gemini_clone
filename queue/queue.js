121693ed45543a7878ef6121b95aae288fc2ade6
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-07-22 12:55:51 +0530

    Update queue.js

--- queue/queue.js
+++ queue/queue.js
@@ -5,6 +5,7 @@ const connection = new IORedis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379,
   maxRetriesPerRequest: null, //  required by BullMQ
+  tls: {},
 });
 
 const messageQueue = new Queue('message-queue', { connection });
