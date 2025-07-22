7d5bee0f36cd6a966b342c301009806a71e022a4
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-07-22 13:04:20 +0530

    Update queue.js

--- queue/queue.js
+++ queue/queue.js
@@ -1,11 +1,16 @@
 const { Queue } = require('bullmq');
 const IORedis = require('ioredis');
 
-const connection = new IORedis({
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: process.env.REDIS_PORT || 6379,
-  maxRetriesPerRequest: null, //  required by BullMQ
-  tls: {},
+// const connection = new IORedis({
+//   host: process.env.REDIS_HOST || '127.0.0.1',
+//   port: process.env.REDIS_PORT || 6379,
+//   maxRetriesPerRequest: null, //  required by BullMQ
+//   tls: {},
+// });
+
+const connection = new IORedis(process.env.UPSTASH_REDIS_URL, {
+  maxRetriesPerRequest: null,
+  tls: {}, // This is required for Upstash (uses HTTPS endpoint)
 });
 
 const messageQueue = new Queue('message-queue', { connection });
